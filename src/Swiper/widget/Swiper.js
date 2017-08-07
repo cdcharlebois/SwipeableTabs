6d14ce361f4c9914e3f9771e726d8fc2fac99594
Conner Charlebois <cdcharlebois@gmail.com>
2017-08-07 17:49:53 -0400

    final functionality

--- src/Swiper/widget/Swiper.js
+++ src/Swiper/widget/Swiper.js
@@ -24,11 +24,15 @@ define([
     return declare("Swiper.widget.Swiper", [_WidgetBase, _TemplatedMixin], {
 
         templateString: template,
-
+        ActiveMarkerBacakgroundColor: null,
+        ShowPagination: null,
+        TabsBackgroundColor: null,
+        ClassToAddToTabs: null,
 
         // Internal variables.
         _handles: null,
         _contextObj: null,
+        _titles: null,
 
         constructor: function() {
             this._handles = [];
@@ -41,27 +45,10 @@ define([
         update: function(obj, callback) {
             logger.debug(this.id + ".update");
             if (obj) this._contextObj = obj;
-            var container = this.domNode.parentElement.querySelector('div.mx-tabcontainer'),
-                wrapper = container.querySelector('div.tab-content'),
-                pagination = container.querySelector('ul.nav-tabs'),
-                slides = wrapper.children,
-                titles = Array.from(pagination.querySelectorAll('li>a')).map(function(el) { return el.innerHTML });
 
-            // add classes
-            dojoClass.add(container, 'swiper-container');
-            dojoClass.add(pagination, 'swiper-pagination');
-            dojoClass.add(wrapper, 'swiper-wrapper');
-            // wrapper.style.marginTop = pagination.getBoundingClientRect().height + 'px';
-            wrapper.style.marginTop = '45px';
+            this._setupDOM();
 
-            Array.from(slides).forEach(function(el) {
-                dojoClass.add(el, 'swiper-slide');
-                el.style.display = 'block';
-                dijit.registry.byId('mxui_widget_TabContainer_0').showTab(dijit.registry.byId(el.id))
-            })
-
-
-            this._updateRendering(callback, titles);
+            this._updateRendering(callback);
         },
 
         resize: function(box) {
@@ -72,51 +59,118 @@ define([
             logger.debug(this.id + ".uninitialize");
         },
 
-        _updateRendering: function(callback, titles) {
+        _updateRendering: function(callback) {
             logger.debug(this.id + "._updateRendering");
             var self = this;
-            var swiper = new Swiper('.swiper-container', {
-                pagination: '.swiper-pagination',
-                slidesPerView: 1,
-                paginationClickable: true,
-                loop: true,
-                paginationBulletRender: function(index, className) {
-                    if (index === (titles.length - 1)) {
-                        return '<span data-index="' + index + '" class="widget-tabnav ' + className + '">' +
-                            titles[index] + '</span>' +
-                            '<div class="active-mark "></div>';
-                    }
-                    return '<span data-index="' + index + '" class="widget-tabnav ' + className + '">' + titles[index] + '</span>';
-                },
-                onSlideChangeStart: function(swiper) {
-                    lang.hitch(self, self._fixActiveMark(titles))
-                }
-            });
-            this._fixActiveMark(titles);
-            this._hidePagination();
 
-            // if (this._contextObj !== null) {
-            //     dojoStyle.set(this.domNode, "display", "block");
-            // } else {
-            //     dojoStyle.set(this.domNode, "display", "none");
-            // }
+            this._initializeSwiper();
 
             this._executeCallback(callback);
         },
 
-        _hidePagination: function() {
-            // this.domNode.parentElement.querySelector('div.mx-tabcontainer').querySelector('ul.nav-tabs').style.display = 'none';
+        /**
+         * Initialize the Swiper
+         * 1. set the render function to render the headers
+         * 2. set a listener to update the Active Mark when the user clicks to show a new tab
+         */
+        _initializeSwiper: function() {
+            var swiper = new Swiper('.swiper-container.' + this.id, {
+                pagination: '.swiper-pagination.' + this.id,
+                slidesPerView: 1,
+                paginationClickable: true,
+                loop: true,
+                paginationBulletRender: lang.hitch(this, function(index, className) {
+                    if (index === (this.titles.length - 1)) {
+                        return '<span data-index="' + index + '" class="widget-tabnav ' + className + '">' +
+                            this.titles[index] + '</span>' +
+                            '<div class="active-mark "></div>';
+                    }
+                    return '<span data-index="' + index + '" class="widget-tabnav ' + className + '">' + this.titles[index] + '</span>';
+                }),
+                onSlideChangeStart: lang.hitch(this, function(swiper) {
+                    lang.hitch(this, this._fixActiveMark())
+                })
+            });
+            this._fixActiveMark();
+            if (!this.ShowPagination) {
+                this._hidePagination();
+            }
+            if (this.TabsBackgroundColor) {
+                this.paginationEl.style.backgroundColor = this.TabsBackgroundColor;
+            }
+
         },
 
-        _fixActiveMark: function(titles) {
-            var pagination = this.domNode.parentElement.querySelector('div.mx-tabcontainer').querySelector('ul.nav-tabs'),
-                totalWidth = pagination.getBoundingClientRect().width,
-                activeMarkWidth = totalWidth / titles.length,
-                selected = document.querySelector('.widget-tabnav.swiper-pagination-bullet-active').dataset.index,
-                activeMarker = pagination.querySelector('.active-mark');
+        /**
+         * Setup the DOM for the Swiper to be initialized.
+         * 1. Add the classes
+         * 2. Render all the Tab Pages 
+         */
+        _setupDOM: function() {
+            var container = this.domNode.parentElement.querySelector('div.mx-tabcontainer'),
+                wrapper = container.querySelector('div.tab-content'),
+                pagination = container.querySelector('ul.nav-tabs'),
+                // limit to only displayed tabs
+                titles = Array.from(pagination.querySelectorAll('li>a'))
+                .filter(function(el) { // only titles for tabs that can be seen
+                    return el.parentElement.style.display != 'none'
+                })
+                .map(function(el) { return el.innerHTML }),
+                slides = Array.from(wrapper.children)
+                .filter(function(el) { return el.style.display != 'none' }); // only slides that can be seen
 
+            // store these variables for retrieval later
+            this.containerEl = container;
+            this.wrapperEl = wrapper;
+            this.paginationEl = pagination;
+            this.slidesEls = slides;
+            this.titles = titles;
+
+            // add classes
+            dojoClass.add(container, 'swiper-container ' + this.id);
+            dojoClass.add(pagination, 'swiper-pagination ' + this.id);
+            dojoClass.add(wrapper, 'swiper-wrapper');
+            // wrapper.style.marginTop = pagination.getBoundingClientRect().height + 'px';
+            wrapper.style.marginTop = '45px';
+
+            // add all the slide classes and render them all
+            Array.from(slides).forEach(function(el) {
+                dojoClass.add(el, 'swiper-slide');
+                el.style.display = 'block';
+                dijit.registry.byId('mxui_widget_TabContainer_0').showTab(dijit.registry.byId(el.id))
+            })
+
+            // 
+            if (this.ClassToAddToTabs) {
+                dojoClass.add(this.paginationEl, this.ClassToAddToTabs)
+            }
+
+        },
+
+
+        /**
+         * Hide the pagination
+         */
+        _hidePagination: function() {
+            this.paginationEl.style.display = 'none';
+        },
+
+        /**
+         * Update the position, width, and color of the active mark
+         */
+        _fixActiveMark: function() {
+            var totalWidth = this.paginationEl.getBoundingClientRect().width,
+                activeMarkWidth = totalWidth / this.titles.length,
+                selected = document.querySelector('.' + this.id + ' .widget-tabnav.swiper-pagination-bullet-active').dataset.index,
+                activeMarker = this.paginationEl.querySelector('.active-mark');
+
+            // update the position, width, and color of the Active Mark
             activeMarker.style.width = activeMarkWidth + 'px';
             activeMarker.style.left = (selected * 1) * activeMarkWidth + 'px';
+            if (this.ActiveMarkerBackgroundColor) {
+                activeMarker.style.backgroundColor = this.ActiveMarkerBackgroundColor;
+            }
+
 
 
         },
